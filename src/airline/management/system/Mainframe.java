3cd6814eb9aea2dd01e5fd60a90cde45b3aa5cc5
Dipraj Howlader <diprajhowlader2015@gmail.com>
2022-02-12 13:56:40 +0600

    first commit

---
+++ src/airline/management/system/Mainframe.java
@@ -0,0 +1,134 @@
+
+
+package airline.management.system;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.*;
+import net.proteanit.sql.DbUtils;
+import java.awt.EventQueue;
+
+
+
+public class Mainframe extends JFrame{
+
+    public static void main(String[] args) {
+        new Mainframe().setVisible(true);
+    }
+    
+    public Mainframe() {
+        super("AIRLINE MANAGEMENT SYSTEM");
+      setForeground(Color.CYAN);
+        setLayout(null); 
+
+        JLabel NewLabel = new JLabel("");
+NewLabel.setIcon(new ImageIcon(ClassLoader.getSystemResource("icons/biman.jpg")));
+//        System.out.println(ClassLoader.getSystemResource(""));
+        NewLabel.setBounds(-1150, 0, 2800, 1400); // x , y, length, breath
+	add(NewLabel); 
+        
+        JLabel AirlineManagementSystem = new JLabel("WELCOME TO BIMAN BANGLADEASH ");
+	AirlineManagementSystem.setForeground(Color.BLUE);
+        AirlineManagementSystem.setFont(new Font("Tahoma", Font.PLAIN, 36));
+	AirlineManagementSystem.setBounds(1500, 60, 1000, 55);
+	NewLabel.add(AirlineManagementSystem);
+		
+		
+        JMenuBar menuBar = new JMenuBar();
+	setJMenuBar(menuBar);
+		
+        JMenu AirlineSystem = new JMenu("AIRLINE SYSTEM");
+        AirlineSystem.setForeground(Color.BLUE);
+	menuBar.add(AirlineSystem);
+		
+        JMenuItem FlightDetails = new JMenuItem("FLIGHT INFO");
+	AirlineSystem.add(FlightDetails);
+		
+	JMenuItem ReservationDetails = new JMenuItem("ADD CUSTOMER DETAILS");
+	AirlineSystem.add(ReservationDetails);
+		
+	JMenuItem PassengerDetails = new JMenuItem("JOURNEY DETAILS");
+	AirlineSystem.add(PassengerDetails);
+		
+
+		
+	JMenuItem Cancellation = new JMenuItem("CANCELLATION");
+	AirlineSystem.add(Cancellation);
+	
+        		
+        JMenu PaymentSystem = new JMenu("PAYMENT SYSTEM");
+        PaymentSystem.setForeground(Color.BLUE);
+	menuBar.add(PaymentSystem);
+
+        JMenuItem Pay = new JMenuItem("PAY");
+        PaymentSystem.add(Pay);
+        
+        JMenuItem SectorDetails_1 = new JMenuItem("PAYMENT DETAILS");
+	PaymentSystem.add(SectorDetails_1);
+	
+	JMenu Ticket = new JMenu("TICKET");
+        Ticket.setForeground(Color.GREEN);
+	menuBar.add(Ticket);
+
+        
+		
+	FlightDetails.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                new Flight_Info();
+            }
+	});
+        
+	ReservationDetails.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {  
+                    new Add_Customer();
+                    
+		} catch (Exception e) {
+                    e.printStackTrace();
+		} 
+            }
+	});
+		
+        PassengerDetails.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {
+                    new Journey_Details();
+                } catch (Exception e) {
+                    e.printStackTrace();
+		}
+            }
+	});
+
+        Cancellation.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                new Cancel();
+            }
+	});
+		
+        SectorDetails_1.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {
+                    new Payment_Details();
+		} catch (Exception e) {
+                    e.printStackTrace();
+		}
+            }
+	});
+
+        Pay.addActionListener(new ActionListener(){
+            public void actionPerformed(ActionEvent ae){
+                try {
+                    new Payment();
+		} catch (Exception e) {
+                    e.printStackTrace();
+		}
+            }
+	});
+		
+        setSize(1950,1090);
+	setVisible(true);
+    }
+
+    
+}
---
+++ src/airline/management/system/Payment.java
@@ -0,0 +1,236 @@
+
+
+package airline.management.system;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.sql.*;
+import javax.swing.*;
+import net.proteanit.sql.DbUtils;
+import java.awt.EventQueue;
+
+public class Payment extends JFrame {
+     JTextField textField,textField_1,textField_2,textField_3,textField_4,textField_5,textField_6,textField_7,textField_8,textField_9,textField_10,textField_11,textField_12,textField_13;
+
+    public static void main(String[] args) {
+        new Payment();
+    }
+
+    public Payment(){
+	initialize();
+    }
+
+    private void initialize(){
+    getContentPane().setForeground(Color.BLUE);
+            getContentPane().setBackground(Color.WHITE);
+            setTitle("GIVE PAYMENT INFO");
+            
+             setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+            setSize(778,486);
+            getContentPane().setLayout(null);
+
+	JLabel FlightDetails = new JLabel("PAYMENT INFO");
+	FlightDetails.setFont(new Font("Tahoma", Font.PLAIN, 36));
+	FlightDetails.setForeground(new Color(100, 149, 237));
+	FlightDetails.setBounds(300, 20, 570, 35);
+	add(FlightDetails);
+                
+            JLabel pnr_no = new JLabel("PASSENGER NO");
+            pnr_no.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            pnr_no.setBounds(60, 80, 150, 27);
+            add(pnr_no);
+            
+            textField = new JTextField();
+            textField.setBounds(200, 80, 150, 27);
+            add(textField);
+            
+            JLabel name = new JLabel("NAME");
+            name.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            name.setBounds(60, 120, 150, 27);
+            add(name);
+            
+            textField_1 = new JTextField();
+            textField_1.setBounds(200, 120, 150, 27);
+            add(textField_1);
+
+            JLabel phone = new JLabel("PHONE");
+            phone.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            phone.setBounds(60, 160, 150, 27);
+            add(phone);
+            
+            textField_2 = new JTextField();
+            textField_2.setBounds(200, 160, 150, 27);
+            add(textField_2);
+
+            JLabel f_code = new JLabel("FLIGHT CODE");
+            f_code.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            f_code.setBounds(60, 200, 150, 27);
+            add(f_code);
+            
+            textField_3 = new JTextField();
+            textField_3.setBounds(200, 200, 150, 27);
+            add(textField_3);
+
+            
+            JLabel ticket_id = new JLabel("TICKET ID");
+            ticket_id.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            ticket_id.setBounds(60, 240, 150, 27);
+            add(ticket_id);
+            
+            textField_4 = new JTextField();
+            textField_4.setBounds(200, 240, 150, 27);
+            add(textField_4);
+
+
+            JLabel jny_date = new JLabel("JOURNEY DATE");
+            jny_date.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            jny_date.setBounds(60, 280, 150, 27);
+            add(jny_date);
+            
+            textField_5 = new JTextField();
+            textField_5.setBounds(200, 280, 150, 27);
+            add(textField_5);
+
+            JLabel jny_time = new JLabel("JOURNEY TIME");
+            jny_time.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            jny_time.setBounds(60, 320, 150, 27);
+            add(jny_time);
+            
+            textField_6 = new JTextField();
+            textField_6.setBounds(200, 320, 150, 27);
+            add(textField_6);
+
+            JLabel src = new JLabel("SOURCE");
+            src.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            src.setBounds(60, 360, 150, 27);
+            add(src);
+            
+            textField_7 = new JTextField();
+            textField_7.setBounds(200, 360, 150, 27);
+            add(textField_7);
+
+
+            JLabel dst = new JLabel("DESTINATION");
+            dst.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            dst.setBounds(60, 400, 150, 27);
+            add(dst);
+            
+            textField_8 = new JTextField();
+            textField_8.setBounds(200, 400, 150, 27);
+            add(textField_8);
+
+            JLabel seat_id = new JLabel("SEAT ID");
+            seat_id.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            seat_id.setBounds(440, 150, 150, 27);
+            add(seat_id);
+            
+            textField_9 = new JTextField();
+            textField_9.setBounds(580, 150, 150, 27);
+            add(textField_9);
+
+// adding button
+            JButton Next = new JButton("SEARCH FOR INFO");
+            Next.setBounds(440, 80, 150, 30);
+            Next.setBackground(Color.BLACK);
+            Next.setForeground(Color.WHITE);
+            add(Next);
+
+            JButton Next_2 = new JButton("CONFIRM PAYMENT");
+            Next_2.setBounds(400, 460, 150, 30);
+            Next_2.setBackground(Color.BLACK);
+            Next_2.setForeground(Color.WHITE);
+            add(Next_2);
+//Again text and text field
+
+            JLabel card_no = new JLabel("CARD NO");
+            card_no.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            card_no.setBounds(440, 220, 150, 27);
+            add(card_no);
+            
+            textField_10 = new JTextField();
+            textField_10.setBounds(580, 220, 150, 27);
+            add(textField_10);
+
+//padi amo
+            JLabel paid_amt = new JLabel("PAID AMOUNT");
+            paid_amt.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            paid_amt.setBounds(440, 270, 150, 27);
+            add(paid_amt);
+            
+            textField_11 = new JTextField();
+            textField_11.setBounds(580, 270, 150, 27);
+            add(textField_11);
+//paydate
+            JLabel pay_date = new JLabel("PAY DATE");
+            pay_date.setFont(new Font("Tahoma", Font.PLAIN, 17));
+            pay_date.setBounds(440, 320, 150, 27);
+            add(pay_date);
+            
+            textField_12 = new JTextField();
+            textField_12.setBounds(580, 320, 150, 27);
+            add(textField_12);
+
+// action listeners
+        Next.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent ae) {
+	
+                String code = textField.getText();
+                
+                try {
+                    conn c = new conn();
+                    String str = "select * from passenger where pnr_no = '"+code+"'";
+      
+                    ResultSet rs = c.s.executeQuery(str);
+                      if(rs.next()){
+                       textField_1.setText(rs.getString("name"));
+                       textField_2.setText(rs.getString("ph_no"));
+                       textField_3.setText(rs.getString("fl_code"));
+}
+                    
+                }catch(SQLException e){
+                    e.printStackTrace();
+                }
+            }
+        });
+
+ Next_2.addActionListener(new ActionListener(){
+                public void actionPerformed(ActionEvent ae){
+                    String pnr_no = textField.getText();
+                    String name = textField_1.getText();
+                    String phone =  textField_2.getText();
+                    String f_code = textField_3.getText();
+                    String ticket_id = textField_4.getText();
+                    String jny_date = textField_5.getText();
+                    String jny_time = textField_6.getText();
+                    String src  = textField_7.getText();
+                    String dst  = textField_8.getText();
+                    String seat_id  = textField_9.getText();
+                    String card_no  = textField_10.getText();
+                    String paid_amt  = textField_11.getText();
+                    String pay_date  = textField_12.getText();
+
+                    
+                    try {
+                        conn c = new conn();
+                        String str = "INSERT INTO reservation values( '"+pnr_no+"', '"+name+"', '"+phone+"','"+ticket_id+"', '"+f_code+"', '"+jny_date+"','"+jny_time+"', '"+src+"','"+dst+"','"+seat_id+"')";
+                        String str2 = "INSERT INTO payment values( '"+pnr_no+"', '"+name+"', '"+phone+"','"+card_no+"', '"+f_code+"', '"+paid_amt+"','"+pay_date+"')";
+                        c.s.executeUpdate(str);
+                        c.s.executeUpdate(str2);
+                        JOptionPane.showMessageDialog(null,"Payment Done"); //pop up
+                        setVisible(false);
+                    
+                    } catch (Exception e) {
+                        e.printStackTrace();
+        	    }
+		}
+            });
+
+
+
+
+	setSize(960,590);
+        setLocation(100,40);
+	setVisible(true);
+
+    }
+}
